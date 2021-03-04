c605cd3346cee2a1122e6610a2c13ca990886595
Fernando Herrera <fernando.herrera85@gmail.com>
2021-03-04 14:30:40 -0500

    Antes de la tarea

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -15,10 +15,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // height: 300,
-        backgroundColor: '#28C4D9',
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end'
+        backgroundColor: '#28C4D9'
     },
     caja1: {
         borderWidth: 2,
@@ -33,6 +30,6 @@ const styles = StyleSheet.create({
     caja3: {
         borderWidth: 2,
         borderColor: 'white',
-        fontSize: 30,
+        fontSize: 30
     },
 });
