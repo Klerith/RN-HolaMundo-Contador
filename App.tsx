949e2603cb084531a92c80eabd799bf47a1c13d1
Fernando Herrera <fernando.herrera85@gmail.com>
2021-03-04 10:47:55 -0500

    antes de useDimensions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
 
 
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* <HolaMundoScreen /> */}
+      {/* <ContadorScreen /> */}
+      <BoxObjectModelScreen />
+
+    </SafeAreaView>
   )
 }
---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.title }>Box Object Model</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    title: {
+        paddingHorizontal: 100,
+        paddingVertical: 20,
+        fontSize: 20,
+        marginHorizontal: 20,
+        // width: 250,
+        borderWidth: 10
+        // backgroundColor: 'red'
+    }
+});
